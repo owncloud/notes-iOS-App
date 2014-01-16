250b8fbbb18392ecbf73cbf36c7a8bbb57d8b107
Peter Hedlund <peter@peterandlinda.com>
2014-01-16 15:50:27 -0800

    Initial Commit

---
+++ iOCNotes/OCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OCAppDelegate.h
+//  iOCNotes
+//
+//  Created by Peter Hedlund on 1/16/14.
+//  Copyright (c) 2014 Peter Hedlund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOCNotes/OCViewController.h
@@ -0,0 +1,13 @@
+//
+//  OCViewController.h
+//  iOCNotes
+//
+//  Created by Peter Hedlund on 1/16/14.
+//  Copyright (c) 2014 Peter Hedlund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OCViewController : UIViewController
+
+@end
