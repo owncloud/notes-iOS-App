dbe4046198ac236488e6bf25bbf1331b7333f819
Peter Hedlund <peter@peterandlinda.com>
2014-02-03 10:38:07 -0800

    Show modification times in a less distracting way. Resolves #7.

--- iOCNotes/OCNotesTableViewController.m
+++ iOCNotes/OCNotesTableViewController.m
@@ -127,19 +127,17 @@
 }
 
 - (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath {
-    //NSIndexPath *indexPathTemp = [NSIndexPath indexPathForRow:indexPath.row inSection:0];
     Note *note = [self.notesFetchedResultsController objectAtIndexPath:indexPath];
     cell.textLabel.text = note.title;
     cell.backgroundColor = [UIColor clearColor];
     NSDate *date = [NSDate dateWithTimeIntervalSince1970:[note.modified doubleValue]];
     if (date) {
         NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
-        dateFormat.dateStyle = NSDateFormatterMediumStyle;
-        dateFormat.timeStyle = NSDateFormatterShortStyle;
-        cell.detailTextLabel.text = [NSString stringWithFormat:@"Modified on %@", [dateFormat stringFromDate:date]];
+        dateFormat.dateStyle = NSDateFormatterShortStyle;
+        dateFormat.timeStyle = NSDateFormatterNoStyle;
+        dateFormat.doesRelativeDateFormatting = YES;
+        cell.detailTextLabel.text = [dateFormat stringFromDate:date];
     }
-
-    //cell.delegate = self;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
