743354fefb0b48d5311da2cabc777c2473b0f758
Peter Hedlund <peter@peterandlinda.com>
2018-06-14 19:44:26 -0700

    Prevent crash if there is a failure in creating a new note on the server.

--- iOCNotes/OCNoteOperationAdd.m
+++ iOCNotes/OCNoteOperationAdd.m
@@ -18,17 +18,24 @@
         [OCAPIClient sharedClient].requestSerializer = [OCAPIClient jsonRequestSerializer];
         [[OCAPIClient sharedClient] POST:@"notes" parameters:params progress:nil success:^(NSURLSessionDataTask *task, id responseObject) {
             if (!self.isCancelled) {
-                NSDictionary *responseDictionary = (NSDictionary*)responseObject;
-                [self.note save:^{
-                    self.note.id = [[responseDictionary objectForKey:@"id"] intValue];
-                    self.note.modified = [[responseDictionary objectForKey:@"modified"] doubleValue];
-                    self.note.title = [responseDictionary objectForKeyNotNull:@"title" fallback:NSLocalizedString(@"New note", @"The title of a new note")];
-                    self.note.addNeeded = NO;
-                    self.note.updateNeeded = NO;
-                }];
-                
-                if (self.delegate) {
-                    [self.delegate noteOperationDidFinish:self];
+                if ([responseObject isKindOfClass:[NSDictionary class]]) {
+                    NSDictionary *responseDictionary = (NSDictionary*)responseObject;
+                    [self.note save:^{
+                        self.note.id = [[responseDictionary objectForKey:@"id"] intValue];
+                        self.note.modified = [[responseDictionary objectForKey:@"modified"] doubleValue];
+                        self.note.title = [responseDictionary objectForKeyNotNull:@"title" fallback:NSLocalizedString(@"New note", @"The title of a new note")];
+                        self.note.addNeeded = NO;
+                        self.note.updateNeeded = NO;
+                    }];
+                    
+                    if (self.delegate) {
+                        [self.delegate noteOperationDidFinish:self];
+                    }
+                } else {
+                    if (self.delegate) {
+                        self.errorMessage = NSLocalizedString(@"Failed to create note on server", @"An error message");
+                        [self.delegate noteOperationDidFail:self];
+                    }
                 }
             }
             [self finish];
