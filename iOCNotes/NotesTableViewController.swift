8469653c085cbd720df9c07f58501f7839ab920a
Peter Hedlund <support@pbh.dev>
2019-11-07 18:36:11 -0800

    Prevent potential crash when adding note.

--- iOCNotes/NotesTableViewController.swift
+++ iOCNotes/NotesTableViewController.swift
@@ -406,7 +406,10 @@ class NotesTableViewController: UITableViewController {
         HUD.show(.progress)
         NotesManager.shared.add(content: "", category: "", completion: { [weak self] note in
             if note != nil {
-                self?.tableView.selectRow(at: IndexPath(row: 0, section: 0), animated: true, scrollPosition: .top)
+                let indexPath = IndexPath(row: 0, section: 0)
+                if self?.notesFrc.validate(indexPath: indexPath) ?? false {
+                    self?.tableView.selectRow(at: indexPath, animated: true, scrollPosition: .top)
+                }
                 self?.performSegue(withIdentifier: detailSegueIdentifier, sender: self)
             }
             HUD.hide()
