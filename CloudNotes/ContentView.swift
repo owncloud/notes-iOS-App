11bd9abb11e05a5ef59c5fad6493b827175b8e7b
Peter Hedlund <support@pbh.dev>
2020-01-03 18:57:17 -0800

    Create a native Mac target.

---
+++ CloudNotes/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  CloudNotes
+//
+//  Created by Peter Hedlund on 1/3/20.
+//  Copyright © 2020 Peter Hedlund. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                HSplitView {
+                    VStack { Text("Pane 1") }.frame(minWidth: 100.0, idealWidth: 250.0, maxWidth: 400.0, maxHeight: .infinity)
+                    VStack { Text("Pane 2") }.frame(minWidth: 200.0, maxWidth: .infinity, maxHeight: .infinity)
+                }
+            }.frame(width: geometry.size.width, height: geometry.size.height)
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
